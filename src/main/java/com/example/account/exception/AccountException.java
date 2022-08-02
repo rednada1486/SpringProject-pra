bfbe2eb499e0e3e464968f70cf41e7f81716bac1
김지섭 <rednada1486@gmail.com>
2022-08-02 23:52:46 +0900

    계좌 생성 관련 Error 반환 클래스 생성

--- src/main/java/com/example/account/exception/AccountException.java
+++ src/main/java/com/example/account/exception/AccountException.java
@@ -1,2 +1,22 @@
-package com.example.account.exception;public class AccountException {
+package com.example.account.exception;
+
+import com.example.account.type.ErrorCode;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+// 그냥 Exception은 checked Exception이라서 메서드의 시그니쳐에 계속 붙이고 다녀야하는 불편함이 있음
+// 또한 트랜잭션의 대상이 되지 않기 때문에 RuntimeException을 상속함.
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String ErrorMessage;
+
+    public AccountException(ErrorCode errorCode){
+        this.errorCode = errorCode;
+        this.ErrorMessage = errorCode.getDescription();
+    }
 }
--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -1,2 +1,12 @@
-package com.example.account.type;public enum ErrorCode {
+package com.example.account.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUNT("사용자가 없습니다.");
+
+    private final String description;
 }
