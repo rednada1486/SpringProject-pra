69f01c339d71dd3771bb975af6d254fc67bbbc1b
김지섭 <rednada1486@gmail.com>
2022-08-02 23:48:15 +0900

    CreatedAt, UpdatedAt 자동 생성을 위한 설정파일 등록

--- src/main/java/com/example/account/config/JpaAuditingConfiguration.java
+++ src/main/java/com/example/account/config/JpaAuditingConfiguration.java
@@ -1,2 +1,11 @@
-package com.example.account.config;public class JpaAuditingConfiguration {
+package com.example.account.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing // 스프링 어플리케이션 뜰 때, 바로 JpaAuditing을 켜줌
+                   // Entity의 CreatedAt 과 UpdatedAt 자동 생성 위한 어노테이션
+public class JpaAuditingConfiguration {
 }
