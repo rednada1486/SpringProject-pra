765f504bb520ab8b6a53e1afe3aa45d279df8804
김지섭 <rednada1486@gmail.com>
2022-08-02 23:50:34 +0900

    CreateAccount(계좌생성 관련 컨트롤러의 데이터 전송 객체) 생성

--- src/main/java/com/example/account/dto/CreateAccount.java
+++ src/main/java/com/example/account/dto/CreateAccount.java
@@ -1,2 +1,38 @@
-package com.example.account.dto;public class CreateAccount {
+package com.example.account.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public class CreateAccount {
+
+
+    @Getter
+    @Setter
+    public static class Request{
+        @NotNull //validation
+        @Min(1) //validation
+        private Long userId;
+
+        @NotNull //validation
+        @Min(100) //validation
+        private Long initialBalance;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response{
+        private Long userId;
+        private String accountNumber;
+        private LocalDateTime registeredAt;
+    }
+
+
+
+
 }
